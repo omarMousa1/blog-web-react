89fa5bae52bb09e58d12263a019b176d53f7752b
Omar Mousa <86836557+omarMousa1@users.noreply.github.com>
2024-09-02 14:12:37 +0300

    Add files via upload

--- src/components/SidebarContent.js
+++ src/components/SidebarContent.js
@@ -1,11 +1,21 @@
-import { NavLink } from "react-router-dom";
-import logo from "../assets/images/logo/logoReact.svg";
+import { Link, NavLink } from "react-router-dom";
+// import logo from "../assets/images/logo/logoReact.svg";
 import { Breadcrumbs } from "./Breadcrumbs";
+import logo2 from "../assets/images/avatar/avatar-man1.svg";
 
 export const SidebarContent = ({ toggleSidebar }) => {
   return (
     <div className="p-4">
-      <img src={logo} alt="logo" />
+      <Link to="/">
+        <div className="flex flex-col justify-center items-center">
+          <div className="border-gray-600 border-2 rounded-full w-12 h-12 overflow-hidden">
+            <img src={logo2} alt="logo" className="object-contain" />
+          </div>
+          <h1 className="border-gray-600 border-b-4 rounded-lg px-1 shadow-sm shadow-black italic">
+            OMAR MOUSA
+          </h1>
+        </div>
+      </Link>
       <Breadcrumbs />
       <div className="flex justify-end mb-4">
         <button className="text-slate-500" onClick={toggleSidebar}>
@@ -14,7 +24,6 @@ export const SidebarContent = ({ toggleSidebar }) => {
       </div>
       <hr />
       <nav className="flex flex-col gap-y-4 mt-5">
-        <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"tictactoe"}>TicTacToe</NavLink>
         <NavLink to={"wordle"}>Wordle</NavLink>
         <NavLink to={"todo"}>Todo List</NavLink>
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Spinner = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-16 h-16 border-4 border-t-black border-l-black border-gray-200 rounded-full animate-spin"></div>
+      <div className="w-12 h-12 border-4 border-t-black border-gray-200 rounded-full animate-spin"></div>
     </div>
   );
 };
