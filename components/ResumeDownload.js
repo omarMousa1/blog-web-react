530d5dfdc5e995ee420fbdd1d454354aee911e10
Omar Mousa <86836557+omarMousa1@users.noreply.github.com>
2025-01-10 00:27:16 +0300

    Add files via upload

--- components/ResumeDownload.js
+++ components/ResumeDownload.js
@@ -1,4 +1,4 @@
-import cv from "../assets/resume/OmarResume.pdf";
+import cv from "../assets/resume/OmarCV.pdf";
 
 export const ResumeDownload = () => {
   const url = cv;
@@ -6,7 +6,7 @@ export const ResumeDownload = () => {
   const downloadResume = () => {
     const link = document.createElement("a");
     link.href = url;
-    link.download = "OmarResume.pdf";
+    link.download = "OmarCV.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
